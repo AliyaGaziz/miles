207fc214f4c9b7477ccc007f6d8f272bfd7a2177
Aliya <ildar2805@gmail.com>
2022-08-16 21:54:01 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String [] args) {
+        int price = 1200;
+        int miles = 1;
+        int rubles = 20;
+        int Bonus = price * miles / rubles;
+        System.out.println(Bonus);
+    }
+}
